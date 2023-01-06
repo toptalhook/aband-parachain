207450d6d58b8566b8c52c1737339aec97a9d257
toptalhook <toptal.cook@gmail.com>
2023-01-06 18:13:14 +0800

    Add license into collators module

--- pallets/collators/src/lib.rs
+++ pallets/collators/src/lib.rs
@@ -1,3 +1,18 @@
+// Copyright 2023 Aband-TEAM.
+// This file is part of substrate-parachain-PoS-template.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(deprecated)]
 
@@ -132,7 +147,6 @@ pub mod pallet {
 		) -> DispatchResultWithPostInfo {
 			T::AuthorityOrigin::ensure_origin(origin)?;
 			ensure!(Self::is_closed_pos(), Error::<T>::ShouldUnderPoA);
-			// fixme
 			Collators::<T>::put(&collators);
 			Self::deposit_event(Event::SetCollators { collators });
 			Ok(().into())
@@ -145,7 +159,6 @@ pub mod pallet {
 		) -> DispatchResultWithPostInfo {
 			T::AuthorityOrigin::ensure_origin(origin)?;
 			ensure!(Self::is_closed_pos(), Error::<T>::ShouldUnderPoA);
-			// fixme
 			Collators::<T>::append(&new_collator);
 			Self::deposit_event(Event::AddCollator { new_collator });
 			Ok(().into())
